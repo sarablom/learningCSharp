63fa363ca993a4425a2a1a399abf0930c5382e74
sarablom <sara.cecilia.blom@gmail.com>
2022-02-28 08:56:33 +0100

    test commits in rider

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WroteLine("Hello world!");
+            Console.WriteLine("Hello world!");
         }
     }
 }
